81ac25b3da421552b98fb76e4552e57453a26bd7
Siqi Hao <siqi@metawatch.org>
2012-10-10 15:55:19 +0300

    Initial Commit

---
+++ MWMApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MWMApp
+//
+//  Created by Siqi Hao on 10/10/12.
+//  Copyright (c) 2012 MetaWatch Oy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MWMApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MWMApp
+//
+//  Created by Siqi Hao on 10/10/12.
+//  Copyright (c) 2012 MetaWatch Oy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
